02fbb249a582213cedd46d1ac08f8e01815e0ee2
Lee <lee@aptitude.co.uk>
2024-05-09 11:51:59 +0100

    Aded Radzen

--- ZauberCMS.Web/Program.cs
+++ ZauberCMS.Web/Program.cs
@@ -1,3 +1,4 @@
+using Radzen;
 using ZauberCMS.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
