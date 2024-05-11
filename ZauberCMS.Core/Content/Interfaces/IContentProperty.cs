793e462c088fda68aa43809f0f682f6e8413f705
Lee <lee@aptitude.co.uk>
2024-05-11 14:05:31 +0100

    Updated content properties display and interaction in admin panel
    Implemented a List Box for better display and user interaction with content properties in the admin panel. Removed some unnecessary ContentTypeProperty fields and added an icon property in IContentProperty for better UI distinction. Also, added functionalities to AddProperty in the CreateContentType component.

--- ZauberCMS.Core/Content/Interfaces/IContentProperty.cs
+++ ZauberCMS.Core/Content/Interfaces/IContentProperty.cs
@@ -7,5 +7,6 @@ public interface IContentProperty
     string Name { get; set; }
     string Alias { get; set; }
     string Value { get; set; }
+    string Icon { get; set; }
     EventCallback<string> ValueChanged { get; set; }
 }
