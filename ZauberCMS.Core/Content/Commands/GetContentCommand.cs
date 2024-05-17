2e0d047f778c0d4493905c9f664efdac8989f6d9
Lee Messenger <lee@aptitude.co.uk>
2024-05-17 16:49:34 +0100

    Working on values from root website in template

--- ZauberCMS.Core/Content/Commands/GetContentCommand.cs
+++ ZauberCMS.Core/Content/Commands/GetContentCommand.cs
@@ -4,9 +4,9 @@ namespace ZauberCMS.Core.Content.Commands;
 
 public class GetContentCommand : IRequest<Models.Content>
 {
-    public Guid Id { get; set; }
-    
+    public Guid? Id { get; set; }
     public bool IncludeChildren { get; set; }
-    
     public bool IncludeParent { get; set; }
+
+    public string ContentTypeAlias { get; set; } = string.Empty;
 }
--- ZauberCMS.Core/Content/Handlers/GetContentHandler.cs
+++ ZauberCMS.Core/Content/Handlers/GetContentHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ZauberCMS.Core.Content.Commands;
 using ZauberCMS.Core.Data;
+using ZauberCMS.Core.Extensions;
 
 namespace ZauberCMS.Core.Content.Handlers;
 
@@ -13,12 +14,12 @@ public class GetContentHandler (IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<ZauberDbContext>();
-        var query = dbContext.Content.AsNoTracking().AsQueryable();
+        var query = dbContext.Content.Include(x => x.ContentType).AsNoTracking().AsQueryable();
         if (request.IncludeParent)
         {
             query = query.Include(x => x.Parent);
         }
-
+        
         if (request.IncludeChildren)
         {
             query = query.Include(x => x.Children);
@@ -29,7 +30,18 @@ public class GetContentHandler (IServiceProvider serviceProvider)
                 query = query.AsSplitQuery();
             }
         }
+
+        if (request.Id != null)
+        {
+            return await query.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
+        }
         
-        return await query.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
+        if (!request.ContentTypeAlias.IsNullOrWhiteSpace())
+        {
+            return await query.FirstOrDefaultAsync(x => x.ContentType.Alias == request.ContentTypeAlias, cancellationToken: cancellationToken);
+        }
+
+        // Should never get here
+        return await query.FirstOrDefaultAsync(cancellationToken: cancellationToken);
     }
 }
