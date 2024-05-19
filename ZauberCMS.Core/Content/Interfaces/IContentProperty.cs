8cf57c7011f68ba57220549020a9598fa53562ce
Lee Messenger <lee@aptitude.co.uk>
2024-05-19 08:18:53 +0100

    Update user interface for content type properties and add description feature
    Updated the user interface in ListContentTypeProperties to show icon and description of each content type property. Added the description field to the IContentProperty interface and its implementations in RichTextEditorContentProperty, TextAreaContentProperty, and TextContentProperty classes. Also added sorting functionality to the SortContent component utilizing RadzenDataGrid.

--- ZauberCMS.Core/Content/Interfaces/IContentProperty.cs
+++ ZauberCMS.Core/Content/Interfaces/IContentProperty.cs
@@ -6,6 +6,7 @@ public interface IContentProperty
 {
     string Name { get; set; }
     string Alias { get; set; }
+    string Description { get; set; }
     string Value { get; set; }
     string Icon { get; set; }
     EventCallback<string> ValueChanged { get; set; }
