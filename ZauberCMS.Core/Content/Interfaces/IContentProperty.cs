03e03b1564ba68da5a9e5a5826807be8e59e5374
Lee <lee@aptitude.co.uk>
2024-05-29 17:17:42 +0100

    Dashboards and sections added

--- ZauberCMS.Core/Content/Interfaces/IContentProperty.cs
+++ ZauberCMS.Core/Content/Interfaces/IContentProperty.cs
@@ -4,11 +4,11 @@ namespace ZauberCMS.Core.Content.Interfaces;
 
 public interface IContentProperty
 {
-    string Name { get; set; }
-    string Alias { get; set; }
-    string Description { get; set; }
-    string Icon { get; set; }
-    public Type? SettingsComponent { get; set; }
+    string Name { get; }
+    string Alias { get; }
+    string Description { get;}
+    string Icon { get; }
+    public Type? SettingsComponent { get; }
 
     string Value { get; set; }
     EventCallback<string> ValueChanged { get; set; }
---
+++ ZauberCMS.Core/Sections/Interfaces/ISectionDashboard.cs
@@ -0,0 +1,8 @@
+﻿namespace ZauberCMS.Core.Sections.Interfaces;
+
+public interface ISectionDashboard
+{
+    string Name { get; }
+    int SortOrder { get; }
+    string SectionAlias { get; }
+}
