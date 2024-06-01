4054a735a26424370ed49fe7d889f7572ba161b5
Lee Messenger <lee@aptitude.co.uk>
2024-06-01 09:21:16 +0100

    Add DialogExtensions and simplify dialog service calls
    Added a new DialogExtensions class which provides a generic method for opening side panels. This simplifies the code by eliminating repetitive dialog service call parameters across all instances. The Radzen.Blazor package has also been added to the ZauberCMS.Core project file.

---
+++ ZauberCMS.Core/Extensions/DialogExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace ZauberCMS.Core.Extensions;
+
+public static class DialogExtensions
+{
+    public static async Task<object> OpenSidePanel<T>(this DialogService dialogService, string title, Dictionary<string, object>? parameters = null, string width = "1200px") where T : ComponentBase
+    {
+        return await dialogService.OpenSideAsync<T>(title, parameters, new SideDialogOptions { CloseDialogOnOverlayClick = true, Position = DialogPosition.Right, ShowMask = true, Width = width});
+    }
+}
