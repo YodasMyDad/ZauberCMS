d4dff55f82c03f5b02db727898aee9268ea67c60
Lee <lee@aptitude.co.uk>
2024-06-20 13:30:25 +0100

    Still working usres section

---
+++ ZauberCMS.Core/Membership/Commands/QueryRolesCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using MediatR;
+using ZauberCMS.Core.Membership.Models;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Core.Membership.Commands;
+
+public class QueryRolesCommand : IRequest<PaginatedList<Role>>
+{
+    public List<string> Roles { get; set; } = [];
+    public bool AsNoTracking { get; set; } = true;
+    public List<Guid> Ids { get; set; } = [];
+    public int PageIndex { get; set; } = 1;
+    public int AmountPerPage { get; set; } = 10;
+    public GetRolesOrderBy OrderBy { get; set; } = GetRolesOrderBy.DateUpdatedDescending;
+    public Expression<Func<Role, bool>>? WhereClause { get; set; }
+    public IQueryable<Role>? Query { get; set; }
+}
+
+public enum GetRolesOrderBy
+{
+    DateUpdated,
+    DateUpdatedDescending,
+    DateCreated,
+    DateCreatedDescending,
+    Name
+}
---
+++ ZauberCMS.Core/Membership/Handlers/QueryRolesHandler.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ZauberCMS.Core.Data;
+using ZauberCMS.Core.Extensions;
+using ZauberCMS.Core.Membership.Commands;
+using ZauberCMS.Core.Membership.Models;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Core.Membership.Handlers;
+
+public class QueryRolesHandler(IServiceProvider serviceProvider)
+    : IRequestHandler<QueryRolesCommand, PaginatedList<Role>>
+{
+    public Task<PaginatedList<Role>> Handle(QueryRolesCommand request, CancellationToken cancellationToken)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ZauberDbContext>();
+        var query = dbContext.Roles.AsQueryable();
+
+        if (request.Query != null)
+        {
+            query = request.Query;
+        }
+        else
+        {
+            
+            if (request.AsNoTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            var idCount = request.Ids.Count;
+            if (request.Ids.Count != 0)
+            {
+                query = query.Where(x => request.Ids.Contains(x.Id));
+                request.AmountPerPage = idCount;
+            }
+        }
+        
+        if (request.WhereClause != null)
+        {
+            query = query.Where(request.WhereClause);
+        }   
+
+        query = request.OrderBy switch
+        {
+            GetRolesOrderBy.DateCreated => query.OrderBy(p => p.DateCreated),
+            GetRolesOrderBy.DateCreatedDescending => query.OrderByDescending(p => p.DateCreated),
+            GetRolesOrderBy.DateUpdated => query.OrderBy(p => p.DateUpdated),
+            GetRolesOrderBy.DateUpdatedDescending => query.OrderByDescending(p => p.DateUpdated),
+            GetRolesOrderBy.Name => query.OrderBy(p => p.Name),
+            _ => query.OrderByDescending(p => p.DateCreated)
+        };
+        
+        return Task.FromResult(query.ToPaginatedList(request.PageIndex, request.AmountPerPage));
+    }
+}
