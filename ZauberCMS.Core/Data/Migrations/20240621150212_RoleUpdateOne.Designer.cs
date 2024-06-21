9ca253977cc281db3318ed279f36ac3c811b7a9b
Lee Messenger <lee@aptitude.co.uk>
2024-06-21 16:14:15 +0100

    Adding properties to roles

--- ZauberCMS.Core/Data/Migrations/20240621150212_RoleUpdateOne.Designer.cs
+++ ZauberCMS.Core/Data/Migrations/20240621150212_RoleUpdateOne.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZauberCMS.Core.Data;
 
@@ -10,9 +11,11 @@ using ZauberCMS.Core.Data;
 namespace ZauberCMS.Core.Data.Migrations
 {
     [DbContext(typeof(ZauberDbContext))]
-    partial class ZauberDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240621150212_RoleUpdateOne")]
+    partial class RoleUpdateOne
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
---
+++ ZauberCMS.Core/Data/Migrations/20240621150212_RoleUpdateOne.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZauberCMS.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleUpdateOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoleProperties",
+                table: "ZauberRoles",
+                newName: "Tabs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Properties",
+                table: "ZauberRoles",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Properties",
+                table: "ZauberRoles");
+
+            migrationBuilder.RenameColumn(
+                name: "Tabs",
+                table: "ZauberRoles",
+                newName: "RoleProperties");
+        }
+    }
+}
