602888b480a4eca6124dbd602fa1b6d246c508b9
Lee Messenger <lee@aptitude.co.uk>
2024-06-30 15:10:53 +0100

    Navigation picker almost done

--- ZauberCMS.Components/Editors/Models/NavigationItem.cs
+++ ZauberCMS.Components/Editors/Models/NavigationItem.cs
@@ -1,12 +1,18 @@
-﻿using ZauberCMS.Core.Extensions;
+﻿using System.ComponentModel.DataAnnotations;
+using ZauberCMS.Core.Extensions;
 
 namespace ZauberCMS.Components.Editors.Models;
 
 public class NavigationItem
 {
     public Guid Id { get; set; } = Guid.NewGuid().NewSequentialGuid();
+    
+    [Required]
     public string? Name { get; set; }
+    
+    [Required]
     public string? Url { get; set; }
+    
     public List<NavigationItem> Children { get; set; } = [];
     public Guid? ContentId { get; set; }
     public int SortOrder { get; set; }
---
+++ ZauberCMS.Core/Extensions/TupleExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace ZauberCMS.Core.Extensions;
+
+public static class TupleExtensions
+{
+    public static void SortList<T>((int oldIndex, int newIndex) indices, List<T> items)
+    {
+        var (oldIndex, newIndex) = indices;
+
+        var itemToMove = items[oldIndex];
+        items.RemoveAt(oldIndex);
+
+        if (newIndex < items.Count)
+        {
+            items.Insert(newIndex, itemToMove);
+        }
+        else
+        {
+            items.Add(itemToMove);
+        }
+    }
+}
