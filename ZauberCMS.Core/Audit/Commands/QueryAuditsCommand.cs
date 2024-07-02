bd281edf395c447195cc7f69cdb81b47d77f185a
Lee <lee@aptitude.co.uk>
2024-07-02 16:13:02 +0100

    Audit log page done

---
+++ ZauberCMS.Core/Audit/Commands/QueryAuditsCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using MediatR;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Core.Audit.Commands;
+
+public class QueryAuditsCommand : IRequest<PaginatedList<Models.Audit>>
+{
+    public bool AsNoTracking { get; set; } = true;
+    public int PageIndex { get; set; } = 1;
+    public int AmountPerPage { get; set; } = 10;
+    
+    public string? Username { get; set; }
+    public GetAuditsOrderBy OrderBy { get; set; } = GetAuditsOrderBy.DateCreatedDescending;
+    public Expression<Func<Models.Audit, bool>>? WhereClause { get; set; }
+    public IQueryable<Models.Audit>? Query { get; set; }
+}
+
+public enum GetAuditsOrderBy
+{
+    DateCreated,
+    DateCreatedDescending,
+    Username
+}
---
+++ ZauberCMS.Core/Audit/Handlers/QueryAuditsHandler.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ZauberCMS.Core.Audit.Commands;
+using ZauberCMS.Core.Data;
+using ZauberCMS.Core.Extensions;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Core.Audit.Handlers;
+
+public class QueryAuditsHandler(IServiceProvider serviceProvider)
+    : IRequestHandler<QueryAuditsCommand, PaginatedList<Models.Audit>>
+{
+    public Task<PaginatedList<Models.Audit>> Handle(QueryAuditsCommand request, CancellationToken cancellationToken)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ZauberDbContext>();
+        var query = dbContext.Audits.AsQueryable();
+
+        if (request.Query != null)
+        {
+            query = request.Query;
+        }
+        else
+        {
+            if (request.AsNoTracking)
+            {
+                query = query.AsNoTracking();
+            }
+        }
+
+        if (request.Username != null)
+        {
+            query = query.Where(x => x.Username == request.Username);
+        }
+        
+        if (request.WhereClause != null)
+        {
+            query = query.Where(request.WhereClause);
+        }   
+        
+        
+        query = request.OrderBy switch
+        {
+            GetAuditsOrderBy.DateCreated => query.OrderBy(p => p.DateCreated),
+            GetAuditsOrderBy.DateCreatedDescending => query.OrderByDescending(p => p.DateCreated),
+            GetAuditsOrderBy.Username => query.OrderBy(p => p.Username),
+            _ => query.OrderByDescending(p => p.DateCreated)
+        };
+        
+        return Task.FromResult(query.ToPaginatedList(request.PageIndex, request.AmountPerPage));
+    }
+}
