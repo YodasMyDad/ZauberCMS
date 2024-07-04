8933b6ca74f7cce622e47a743def905e3e90279e
Lee <lee@aptitude.co.uk>
2024-07-04 10:55:55 +0100

    Internal redirect id and improving performance of initial check on catch all

--- ZauberCMS.Core/Content/Handlers/GetContentBySlugHandler.cs
+++ ZauberCMS.Core/Content/Handlers/GetContentBySlugHandler.cs
@@ -9,9 +9,64 @@ namespace ZauberCMS.Core.Content.Handlers;
 public class GetContentBySlugHandler(IServiceProvider serviceProvider)
     : IRequestHandler<GetContentBySlugCommand, Models.Content?>
 {
+    
     public async Task<Models.Content?> Handle(GetContentBySlugCommand request, CancellationToken cancellationToken)
     {
-        //TODO - Need to sanitise and check slug
+        // TODO - Need to sanitize and check slug
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ZauberDbContext>();
+
+        // If this is root content, get the first one with minimal data
+        var content = request.IsRootContent
+            ? await dbContext.Contents
+                .AsNoTracking()
+                .Where(c => c.IsRootContent)
+                .Select(c => new { c.Id, c.InternalRedirectId })
+                .FirstOrDefaultAsync(cancellationToken: cancellationToken)
+            : await dbContext.Contents
+                .AsNoTracking()
+                .Where(c => c.Url == request.Slug)
+                .Select(c => new { c.Id, c.InternalRedirectId })
+                .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+        // If this content has an internal redirect id, get that content's ID instead
+        if (content?.InternalRedirectId != null && content.InternalRedirectId != Guid.Empty && !request.IgnoreInternalRedirect)
+        {
+            var internalRedirectIdValue = content.InternalRedirectId.Value;
+            content = await dbContext.Contents
+                .AsNoTracking()
+                .Where(c => c.Id == internalRedirectIdValue)
+                .Select(c => new { c.Id, c.InternalRedirectId })
+                .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+        }
+
+        if (content == null)
+        {
+            return null;
+        }
+
+        // Now we perform the more expensive query to fetch the content with includes
+        var query = dbContext.Contents
+            .AsNoTracking()
+            .Include(x => x.PropertyData)
+            .Include(x => x.Parent)
+            .Include(x => x.ContentType)
+            .AsSplitQuery()
+            .AsQueryable();
+
+        if (request.IncludeChildren)
+        {
+            query = query.Include(x => x.Children);
+        }
+
+        var fullContent = await query
+            .FirstOrDefaultAsync(c => c.Id == content.Id, cancellationToken: cancellationToken);
+
+        return fullContent;
+    }
+    
+    /*public async Task<Models.Content?> Handle(GetContentBySlugCommand request, CancellationToken cancellationToken)
+    {
         using var scope = serviceProvider.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<ZauberDbContext>();
         
@@ -37,12 +92,12 @@ public class GetContentBySlugHandler(IServiceProvider serviceProvider)
                 .FirstOrDefaultAsync(c => c.Url == request.Slug, cancellationToken: cancellationToken);
 
         // If this content has an internal redirect id, return that content instead
-        if (content?.InternalRedirectId != null && !request.IgnoreInternalRedirect)
+        if (content?.InternalRedirectId != null && content.InternalRedirectId != Guid.Empty && !request.IgnoreInternalRedirect)
         {
             content = await query
                 .FirstOrDefaultAsync(c => c.Id == content.InternalRedirectId.Value, cancellationToken: cancellationToken);
         }
         
         return content;
-    }
+    }*/
 }
