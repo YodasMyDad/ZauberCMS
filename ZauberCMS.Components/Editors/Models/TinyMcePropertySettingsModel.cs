79a17d14454c58f665e53a8d273669a6c432cf56
Lee Messenger <lee@aptitude.co.uk>
2024-07-12 15:36:34 +0100

    TinyMCE Settings added

---
+++ ZauberCMS.Components/Editors/Models/TinyMcePropertySettingsModel.cs
@@ -0,0 +1,58 @@
+﻿namespace ZauberCMS.Components.Editors.Models;
+
+public class TinyMcePropertySettingsModel
+{
+    public TinyMcePropertySettingsModel()
+    {
+        SelectedMenuBar = MenuBarList;
+        SelectedToolBar = ToolBarList;
+    }
+    
+    public bool ShowMenuBar { get; set; } = true;
+    public List<string> SelectedMenuBar { get; set; }
+    public List<string> SelectedToolBar { get; set; }
+    
+    public List<string> MenuBarList { get; set; } =
+    [
+        "file",
+        "edit",
+        "insert",
+        "view",
+        "format",
+        "table",
+        "tools"
+    ];
+    
+    public List<string> ToolBarList { get; set; } =
+    [
+        "undo",
+        "redo",
+        "fontfamily",
+        "fontsize",
+        "bold",
+        "italic",
+        "underline",
+        "strikethrough",
+        "align",
+        "numlist",
+        "bullist",
+        "link",
+        "image",
+        "table",
+        "lineheight",
+        "outdent",
+        "indent",
+        "forecolor",
+        "backcolor",
+        "removeformat",
+        "charmap",
+        "emoticons",
+        "code",
+        "fullscreen",
+        "preview",
+        "anchor",
+        "codesample",
+        "ltr",
+        "rtl"
+    ];
+}
