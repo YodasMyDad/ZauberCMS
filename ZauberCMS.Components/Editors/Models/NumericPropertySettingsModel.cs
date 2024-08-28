7d99baa3878b4a8d4116b875427373ca807c3d30
Lee <lee@aptitude.co.uk>
2024-08-28 16:10:10 +0100

    Adding settings to NumericProperty.razor

---
+++ ZauberCMS.Components/Editors/Models/NumericPropertySettingsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ZauberCMS.Components.Editors.Models;
+
+public class NumericPropertySettingsModel
+{
+    public decimal? Min { get; set; }
+    public decimal? Max { get; set; }
+    public string? Step { get; set; }
+    public string? Placeholder { get; set; }
+    public string? Format { get; set; }
+    public bool AlignRight { get; set; }
+}
