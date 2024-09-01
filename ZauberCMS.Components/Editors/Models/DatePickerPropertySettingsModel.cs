47bab075bf0d3346e76e598cf9142d73c8fa532a
Lee Messenger <lee@aptitude.co.uk>
2024-09-01 09:07:59 +0100

    Adding settings and configs to date picker

---
+++ ZauberCMS.Components/Editors/Models/DatePickerPropertySettingsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ZauberCMS.Components.Editors.Models;
+
+public class DatePickerPropertySettingsModel
+{
+    public bool ShowTimeOnly { get; set; }
+    public DateTime? MinDate { get; set; }
+    public DateTime? MaxDate { get; set; }
+    public bool InlineCalendar { get; set; }
+}
