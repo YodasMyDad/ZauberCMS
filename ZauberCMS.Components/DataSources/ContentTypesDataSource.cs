970f2d13a7e347125c1adce1ef8ef2dd33d9bf78
Lee <lee@aptitude.co.uk>
2024-09-04 16:52:18 +0100

    Editor notes completed

---
+++ ZauberCMS.Components/DataSources/ContentTypesDataSource.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using ZauberCMS.Core.Content.Commands;
+using ZauberCMS.Core.Content.Interfaces;
+using ZauberCMS.Core.Content.Models;
+
+namespace ZauberCMS.Components.DataSources;
+
+public class ContentTypesDataSource(IMediator mediator) : IDataListSource
+{
+    public string Name => "Content Types";
+    public string Description => "List of all content types";
+    public string Icon => "content_copy";
+    public string FullName => GetType().FullName ?? string.Empty;
+
+    public IEnumerable<DataListItem> GetItems(IServiceScope scope, Content? currentContent)
+    {
+        var contentTypes = mediator.Send(new QueryContentTypesCommand
+        {
+            OrderBy = GetContentTypesOrderBy.Name,
+            AmountPerPage = 300,
+            AsNoTracking = true
+        }).GetAwaiter().GetResult();
+        
+        return contentTypes.Items.Select(x => new DataListItem
+            {
+                // Get the name of the component
+                Name = x.Name!,
+                // Get the full name of the component
+                Value = x.Id.ToString(),
+            })
+            .OrderBy(x => x.Name)
+            .ToList();
+    }
+}
---
+++ ZauberCMS.Components/Editors/Models/EditorNotesSettingsModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ZauberCMS.Components.Editors.Models;
+
+public class EditorNotesSettingsModel
+{
+    /// <summary>
+    /// Optional background colour override
+    /// </summary>
+    public string? BackgroundColor { get; set; }
+    
+    /// <summary>
+    /// Title
+    /// </summary>
+    public string? Title { get; set; }
+    
+    /// <summary>
+    /// Main note to editor
+    /// </summary>
+    public string? Note { get; set; }
+
+    /// <summary>
+    /// Optional icon override
+    /// </summary>
+    public string? Icon { get; set; } = "lightbulb_outline";
+    
+    /// <summary>
+    /// Defaults to dialog, but can be made to show inline
+    /// </summary>
+    public bool ShowInline { get; set; }
+}
