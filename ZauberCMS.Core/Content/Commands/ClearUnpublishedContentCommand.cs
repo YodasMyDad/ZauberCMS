a24dca813caa24654f4c54df6d49b2ecf0afff1b
Lee <lee@aptitude.co.uk>
2024-09-04 12:36:37 +0100

    Updated to easily see unpublished content Made it easier to reset unpublished content UI updates when changing from published to unpublished

---
+++ ZauberCMS.Core/Content/Commands/ClearUnpublishedContentCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using ZauberCMS.Core.Content.Models;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Core.Content.Commands;
+
+public class ClearUnpublishedContentCommand : IRequest<HandlerResult<UnpublishedContent>>
+{
+    public Guid ContentId { get; set; }
+}
---
+++ ZauberCMS.Core/Content/Handlers/ClearUnpublishedContentHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ZauberCMS.Core.Content.Commands;
+using ZauberCMS.Core.Content.Models;
+using ZauberCMS.Core.Data;
+using ZauberCMS.Core.Extensions;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Core.Content.Handlers;
+
+public class ClearUnpublishedContentHandler(IServiceProvider serviceProvider) : IRequestHandler<ClearUnpublishedContentCommand, HandlerResult<UnpublishedContent>>
+{
+    public async Task<HandlerResult<UnpublishedContent>> Handle(ClearUnpublishedContentCommand request, CancellationToken cancellationToken)
+    { 
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ZauberDbContext>();
+        var handlerResult = new HandlerResult<UnpublishedContent>();
+        var content = await dbContext.Contents.FirstOrDefaultAsync(x => x.Id == request.ContentId, cancellationToken: cancellationToken);
+        if (content?.UnpublishedContentId != null)
+        {
+            var uContent = await dbContext.UnpublishedContent.FirstOrDefaultAsync(x => x.Id == content.UnpublishedContentId, cancellationToken: cancellationToken);
+            if (uContent != null) dbContext.UnpublishedContent.Remove(uContent);
+            var result = (await dbContext.SaveChangesAndLog(null, handlerResult, cancellationToken))!;
+            return result;
+        }
+        return handlerResult;
+    }
+}
