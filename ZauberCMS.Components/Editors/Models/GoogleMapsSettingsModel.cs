098aa470a3e5712a2f16bfd24994a51bca301949
Lee <lee@aptitude.co.uk>
2024-09-09 14:30:20 +0100

    Google maps property editor added

---
+++ ZauberCMS.Components/Editors/Models/GoogleMapsSettingsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ZauberCMS.Components.Editors.Models;
+
+public class GoogleMapsSettingsModel
+{
+    public double? DefaultLat { get; set; } = 51.5074;
+    public double? DefaultLng { get; set; } = 0.1278;
+    public int ZoomLevel { get; set; } = 3;
+}
