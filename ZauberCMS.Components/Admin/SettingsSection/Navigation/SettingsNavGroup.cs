783b231608fdf767dda3dafc6bdcb1f32f3ca218
Lee Messenger <lee@aptitude.co.uk>
2024-09-20 08:32:27 +0100

    Case rename

--- ZauberCMS.Components/Admin/SettingsSection/Navigation/SettingsNavGroup.cs
+++ ZauberCMS.Components/Admin/SettingsSection/Navigation/SettingsNavGroup.cs
@@ -4,9 +4,9 @@ using ZauberCMS.Core.Sections.Interfaces;
 
 namespace ZauberCMS.Components.Admin.SettingsSection.Navigation;
 
-public class SettingsNavGroup(IStringLocalizer<SharedResource> Localizer) : ISectionNavGroup
+public class SettingsNavGroup(IStringLocalizer<SharedResource> localizer) : ISectionNavGroup
 {
-    public string Heading => Localizer["Heading.Settings"];
+    public string Heading => localizer["Heading.Settings"];
     public string Alias => Constants.Sections.SectionNavGroups.SettingsNavGroup;
     public int SortOrder => 10;
     public string SectionAlias => Constants.Sections.SettingsSection;
