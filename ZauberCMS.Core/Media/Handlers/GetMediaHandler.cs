d667bf96457326f756d3c419788364b65fee610e
Lee <lee@aptitude.co.uk>
2024-10-03 10:41:31 +0100

    Adding caching to get media Updating Template

--- ZauberCMS.Core/Media/Handlers/GetMediaHandler.cs
+++ ZauberCMS.Core/Media/Handlers/GetMediaHandler.cs
@@ -1,54 +1,80 @@
-﻿using MediatR;
+﻿using System.Security.Cryptography;
+using System.Text;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ZauberCMS.Core.Data;
+using ZauberCMS.Core.Extensions;
 using ZauberCMS.Core.Media.Commands;
+using ZauberCMS.Core.Shared.Services;
 
 namespace ZauberCMS.Core.Media.Handlers;
 
-public class GetMediaHandler(IServiceProvider serviceProvider)
+public class GetMediaHandler(IServiceProvider serviceProvider, ICacheService cacheService)
     : IRequestHandler<GetMediaCommand, Models.Media?>
 {
     public async Task<Models.Media?> Handle(GetMediaCommand request, CancellationToken cancellationToken)
     {
-        using (var scope = serviceProvider.CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<ZauberDbContext>();
-            var query = dbContext.Medias.AsQueryable();
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ZauberDbContext>();
+        var cacheKey = GenerateCacheKey(request, dbContext);
 
-            if (request.AsNoTracking)
-            {
-                query = query.AsNoTracking();
-            }
+        if (request.Cached)
+        {
+            return await cacheService.GetSetCachedItemAsync(cacheKey, async () => await FetchMediaAsync(request, dbContext, cancellationToken));
+        }
+
+        return await FetchMediaAsync(request, dbContext, cancellationToken);
+    }
+
+    private static string GenerateCacheKey(GetMediaCommand request, ZauberDbContext dbContext)
+    {
+        var query = BuildQuery(request, dbContext);
+        var queryString = query.ToQueryString();
+        var hash = SHA256.HashData(Encoding.UTF8.GetBytes(queryString));
+        return typeof(Models.Media).ToCacheKey(Convert.ToBase64String(hash));
+    }
+
+    private static IQueryable<Models.Media> BuildQuery(GetMediaCommand request, ZauberDbContext dbContext)
+    {
+        var query = dbContext.Medias.AsQueryable();
+
+        if (request.AsNoTracking)
+        {
+            query = query.AsNoTracking();
+        }
+
+        if (request.IncludeParent)
+        {
+            query = query.Include(x => x.Parent);
+        }
+
+        if (request.IncludeChildren)
+        {
+            query = query.Include(x => x.Children);
 
             if (request.IncludeParent)
             {
-                query = query.Include(x => x.Parent);
+                query = query.AsSplitQuery();
             }
-
-            if (request.IncludeChildren)
-            {
-                query = query.Include(x => x.Children);
-
-                if (request.IncludeParent)
-                {
-                    // Make it a split query if parent is also included
-                    query = query.AsSplitQuery();
-                }
-            }
-
-            if (request.MediaType != null)
-            {
-                query = query.Where(x => x.MediaType == request.MediaType);
-            }
-
-            if (request.Id != null)
-            {
-                return await query.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
-            }
-
-            // Should never get here
-            return await query.FirstOrDefaultAsync(cancellationToken: cancellationToken);
         }
+
+        if (request.MediaType != null)
+        {
+            query = query.Where(x => x.MediaType == request.MediaType);
+        }
+
+        if (request.Id != null)
+        {
+            query = query.Where(x => x.Id == request.Id);
+        }
+
+        return query;
+    }
+
+    private static async Task<Models.Media?> FetchMediaAsync(GetMediaCommand request, ZauberDbContext dbContext, CancellationToken cancellationToken)
+    {
+        var query = BuildQuery(request, dbContext);
+        return await query.FirstOrDefaultAsync(cancellationToken: cancellationToken);
     }
 }
