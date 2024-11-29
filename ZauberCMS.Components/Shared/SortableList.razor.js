82c961a13af12753ef75dac3e2ceebe7cdcd90c3
Lee Messenger <lee@aptitude.co.uk>
2024-11-29 08:49:42 +0000

    Sortable working with shadow dom now

--- ZauberCMS.Components/Shared/SortableList.razor.js
+++ ZauberCMS.Components/Shared/SortableList.razor.js
@@ -1,4 +1,4 @@
-export function init(id, group, pull, put, sort, handle, filter, component, forceFallback) {
+export function init(id, group, pull, put, sort, handle, filter, component, forceFallback, ignoreDomChanges) {
     var sortable = new Sortable(document.getElementById(id), {
         animation: 200,
         group: {
@@ -12,23 +12,13 @@ export function init(id, group, pull, put, sort, handle, filter, component, forc
         handle: handle || undefined,
         onUpdate: (event) => {
             // Revert the DOM to match the .NET state
-            event.item.remove();
-            event.to.insertBefore(event.item, event.to.childNodes[event.oldIndex]);
-
+            if(!ignoreDomChanges){
+                event.item.remove();
+                event.to.insertBefore(event.item, event.to.childNodes[event.oldIndex]);    
+            }
+            
             // Notify .NET to update its model and re-render
             component.invokeMethodAsync('OnUpdateJS', event.oldDraggableIndex, event.newDraggableIndex);
-        },
-        onRemove: (event) => {
-            if (event.pullMode === 'clone') {
-                // Remove the clone
-                event.clone.remove();
-            }
-
-            event.item.remove();
-            event.from.insertBefore(event.item, event.from.childNodes[event.oldIndex]);
-
-            // Notify .NET to update its model and re-render
-            component.invokeMethodAsync('OnRemoveJS', event.oldDraggableIndex, event.newDraggableIndex);
         }
     });
 }
