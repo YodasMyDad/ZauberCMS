741f2da9ea60ea4a8472399f764934adfade03cd
Lee Messenger <lee@aptitude.co.uk>
2025-02-17 17:38:16 +0000

    Add context menu for creating languages in settings tree
    Introduced `CreateLanguageContextMenu` to handle context menu actions for language creation in the settings tree. Refactored `SettingsLanguagesTree` to remove inline context menu logic, delegating it to the new class for better modularity and maintainability. Simplified dependency injection and cleaned up unused code.

---
+++ ZauberCMS.Components/Trees/ContextMenus/SettingsMenus/CreateLanguageContextMenu.cs
@@ -0,0 +1,47 @@
+﻿using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using ZauberCMS.Components.Admin.SettingsSection.Dialogs;
+using ZauberCMS.Core;
+using ZauberCMS.Core.Extensions;
+using ZauberCMS.Core.Languages.Models;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Components.Trees.ContextMenus.SettingsMenus;
+
+public class CreateLanguageContextMenu() : ITreeContextMenu
+{
+    public List<string> Sections { get; } = [];
+    public List<string> TreeAlias { get; } = [Constants.Sections.Trees.SettingsLanguagesTree];
+    public string Text(TreeItemContextMenuEventArgs args) => "Create";
+
+    public string Icon(TreeItemContextMenuEventArgs args) => string.Empty;
+
+    public string IconColor(TreeItemContextMenuEventArgs args) => string.Empty;
+
+    public bool CanShowContextMenu(TreeItemContextMenuEventArgs args)
+    {
+        if (args.Value is TreeStub treeStub)
+        {
+            if (treeStub.StubType == typeof(Language))
+            {
+                return true;   
+            }
+        }
+        return false;
+    }
+
+    public async Task ContextMenuAction(TreeItemContextMenuEventArgs args, MenuItemEventArgs e, NavigationManager navigationManager,
+        ContextMenuService contextMenuService, IModalService modalService)
+    {
+        contextMenuService.Close();
+        var dialog = modalService.OpenSidePanel<CreateLanguage>("Create Language");
+        var result = await dialog.Result;
+        if (result is { Confirmed: true, Data: Language language })
+        {
+            // Do I need to do anything else here?
+        }      
+    }
+
+    public int SortOrder => -90;
+}
