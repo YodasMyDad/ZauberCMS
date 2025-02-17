c2c5739bef69d162912fb3adbf490ee1bb54c8dc
Lee Messenger <lee@aptitude.co.uk>
2025-02-17 17:32:35 +0000

    Adding context menu

---
+++ ZauberCMS.Components/Trees/ContextMenus/SettingsMenus/CopyContentTypeContextMenu.cs
@@ -0,0 +1,42 @@
+﻿using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using ZauberCMS.Core;
+using ZauberCMS.Core.Content.Models;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Components.Trees.ContextMenus.SettingsMenus;
+
+public class CopyContentTypeContextMenu() : ITreeContextMenu
+{
+    public List<string> Sections { get; } = [];
+    public List<string> TreeAlias { get; } = [Constants.Sections.Trees.SettingsStructureTree];
+    public string Text(TreeItemContextMenuEventArgs args) => "Copy";
+
+    public string Icon(TreeItemContextMenuEventArgs args) => string.Empty;
+
+    public string IconColor(TreeItemContextMenuEventArgs args) => string.Empty;
+
+    public bool CanShowContextMenu(TreeItemContextMenuEventArgs args)
+    {
+        if (args.Value is TreeBranch treeBranch)
+        {
+            if (treeBranch.BranchType == typeof(ContentType))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Task ContextMenuAction(TreeItemContextMenuEventArgs args, MenuItemEventArgs e, NavigationManager navigationManager,
+        ContextMenuService contextMenuService, IModalService modalService)
+    {
+        var branch = (TreeBranch)args.Value;
+        contextMenuService.Close();
+        navigationManager.NavigateTo($"/admin/copycontentype/{branch.Id}");
+        return Task.CompletedTask;
+    }
+
+    public int SortOrder => -100;
+}
---
+++ ZauberCMS.Components/Trees/ContextMenus/SettingsMenus/CreateContentTypeContextMenu.cs
@@ -0,0 +1,41 @@
+﻿using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using ZauberCMS.Core;
+using ZauberCMS.Core.Content.Models;
+using ZauberCMS.Core.Shared.Models;
+
+namespace ZauberCMS.Components.Trees.ContextMenus.SettingsMenus;
+
+public class CreateContentTypeContextMenu() : ITreeContextMenu
+{
+    public List<string> Sections { get; } = [];
+    public List<string> TreeAlias { get; } = [Constants.Sections.Trees.SettingsStructureTree];
+    public string Text(TreeItemContextMenuEventArgs args) => "Create";
+
+    public string Icon(TreeItemContextMenuEventArgs args) => string.Empty;
+
+    public string IconColor(TreeItemContextMenuEventArgs args) => string.Empty;
+
+    public bool CanShowContextMenu(TreeItemContextMenuEventArgs args)
+    {
+        if (args.Value is TreeStub treeStub)
+        {
+            if (treeStub.StubType == typeof(ContentType))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Task ContextMenuAction(TreeItemContextMenuEventArgs args, MenuItemEventArgs e, NavigationManager navigationManager,
+        ContextMenuService contextMenuService, IModalService modalService)
+    {
+        contextMenuService.Close();
+        navigationManager.NavigateTo("/admin/createcontentype");
+        return Task.CompletedTask;
+    }
+
+    public int SortOrder => -90;
+}
