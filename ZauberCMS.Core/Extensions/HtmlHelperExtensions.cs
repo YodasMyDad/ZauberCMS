2df6f771b48c70e587202a3b1f045feccf183b2b
Lee <lee@aptitude.co.uk>
2025-02-18 12:38:56 +0000

    Refactor meta tag generation in HtmlHelper extensions.
    Extracted logic for rendering meta tags into a dedicated method to improve reusability and readability. Enhanced support for Content models alongside ZauberPageViewModel. Added detection for home page URLs to ensure accurate canonical and Open Graph tags.

--- ZauberCMS.Core/Extensions/HtmlHelperExtensions.cs
+++ ZauberCMS.Core/Extensions/HtmlHelperExtensions.cs
@@ -3,7 +3,7 @@ using System.Web;
 using MediatR;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using ZauberCMS.Core.Media.Commands;
+using ZauberCMS.Core.Content.Interfaces;
 using ZauberCMS.Core.Rendering;
 using ZauberCMS.Core.Seo.Models;
 
@@ -11,61 +11,86 @@ namespace ZauberCMS.Core.Extensions;
 
 public static class HtmlHelperExtensions
 {
-    public static async Task<IHtmlContent> GenerateMetaTags(this IHtmlHelper<dynamic> htmlHelper, string seoAlias, IMediator mediator, string? titleAlias = null, string? descriptionAlias = null)
+    public static async Task<IHtmlContent> GenerateMetaTags(this IHtmlHelper<dynamic> htmlHelper, string seoAlias,
+        IMediator mediator, string? titleAlias = null, string? descriptionAlias = null)
     {
-        if (htmlHelper.ViewData.Model is ZauberPageViewModel model)
+        if (htmlHelper.ViewData.Model is ZauberPageViewModel or Content.Models.Content)
         {
             var sb = new StringBuilder();
 
-            var metaData = model.GetValue<Meta>(seoAlias);
-            if (metaData != null)
+            if (htmlHelper.ViewData.Model is ZauberPageViewModel viewModel)
             {
-                var domain = htmlHelper.ViewContext.HttpContext.Request.Scheme + "://" + htmlHelper.ViewContext.HttpContext.Request.Host;
-                
-                // Basic meta tags
-                sb.AppendLine($"<title>{HttpUtility.HtmlEncode(metaData.PageTitle ?? model.Name)}</title>");
-                AddMetaTag(sb, "description", metaData.MetaDescription);
-
-                if (metaData.OpenGraphImage != null)
-                {
-                    var result = await mediator.GetMedia(metaData.OpenGraphImage.Value);
-                    if (result != null)
-                    {
-                        AddOpenGraphTag(sb, "og:image", $"{domain}/{result.Url}?width=1200&height=630&mode=stretch");
-                        AddOpenGraphTag(sb, "og:image:width", "1200");
-                        AddOpenGraphTag(sb, "og:image:height", "630");
-                        AddOpenGraphTag(sb, "og:image:type", "image/jpeg");
-                        AddMetaTag(sb, "twitter:card", "summary_large_image");
-                    }
-                }
-                AddOpenGraphTag(sb, "og:locale", System.Globalization.CultureInfo.CurrentCulture.Name);
-                AddOpenGraphTag(sb, "og:title", metaData.PageTitle ?? model.Name);
-                AddOpenGraphTag(sb, "og:description", metaData.MetaDescription);
-                AddOpenGraphTag(sb, "og:url", $"{domain}/{model.Url}");
-                
-                AddMetaTag(sb, "robots",
-                    metaData.HideFromSearchEngines ? "noindex" : "index, follow, max-image-preview:large");
-
-                AddLinkTag(sb, "canonical", $"{domain}/{model.Url}");
+                await RenderTags(htmlHelper, sb, mediator, seoAlias, viewModel.Name ?? string.Empty,
+                    viewModel.Url, viewModel, titleAlias, descriptionAlias);
             }
-            else
+            else if (htmlHelper.ViewData.Model is Content.Models.Content contentModel)
             {
-                // Basic meta tags
-                sb.AppendLine($"<title>{HttpUtility.HtmlEncode(model.GetValue<string>(titleAlias ?? "") ?? model.Name)}</title>");
-                AddMetaTag(sb, "description", model.GetValue<string>(descriptionAlias ?? ""));   
+                await RenderTags(htmlHelper, sb, mediator, seoAlias, contentModel.Name ?? string.Empty,
+                    contentModel.Url, contentModel, titleAlias, descriptionAlias);
             }
             
             return new HtmlString(sb.ToString());
         }
-        
+
         return new HtmlString(string.Empty);
+    }
+
+    private static async Task RenderTags(IHtmlHelper<dynamic> htmlHelper, StringBuilder sb, IMediator mediator,
+        string seoAlias, string name, string? url, IHasPropertyValues model, string? titleAlias = null,
+        string? descriptionAlias = null)
+    {
+        var metaData = model.GetValue<Meta>(seoAlias);
+        if (metaData != null)
+        {
+            
+            var httpContext = htmlHelper.ViewContext.HttpContext;
+            var domain = httpContext.Request.Scheme + "://" + httpContext.Request.Host;
+
+            // Determine if this is the home page using HttpContext
+            var isHomePage = httpContext.Request.Path == "/";
+            
+            // Basic meta tags
+            sb.AppendLine($"<title>{HttpUtility.HtmlEncode(metaData.PageTitle ?? name)}</title>");
+            AddMetaTag(sb, "description", metaData.MetaDescription);
+
+            if (metaData.OpenGraphImage != null)
+            {
+                var result = await mediator.GetMedia(metaData.OpenGraphImage.Value);
+                if (result != null)
+                {
+                    AddOpenGraphTag(sb, "og:image", $"{domain}/{result.Url}?width=1200&height=630&mode=stretch");
+                    AddOpenGraphTag(sb, "og:image:width", "1200");
+                    AddOpenGraphTag(sb, "og:image:height", "630");
+                    AddOpenGraphTag(sb, "og:image:type", "image/jpeg");
+                    AddMetaTag(sb, "twitter:card", "summary_large_image");
+                }
+            }
+
+            AddOpenGraphTag(sb, "og:locale", System.Globalization.CultureInfo.CurrentCulture.Name);
+            AddOpenGraphTag(sb, "og:title", metaData.PageTitle ?? name);
+            AddOpenGraphTag(sb, "og:description", metaData.MetaDescription);
+            AddOpenGraphTag(sb, "og:url", isHomePage ? domain : $"{domain}/{url}");
+
+            AddMetaTag(sb, "robots",
+                metaData.HideFromSearchEngines ? "noindex" : "index, follow, max-image-preview:large");
+            
+            AddLinkTag(sb, "canonical", isHomePage ? domain : $"{domain}/{url}");
+
+        }
+        else
+        {
+            // Basic meta tags
+            sb.AppendLine($"<title>{HttpUtility.HtmlEncode(model.GetValue<string>(titleAlias ?? "") ?? name)}</title>");
+            AddMetaTag(sb, "description", model.GetValue<string>(descriptionAlias ?? ""));
+        }
     }
 
     private static void AddMetaTag(StringBuilder sb, string name, string? content)
     {
         if (!string.IsNullOrWhiteSpace(content))
         {
-            sb.AppendLine($"<meta name=\"{HttpUtility.HtmlEncode(name)}\" content=\"{HttpUtility.HtmlEncode(content)}\">");
+            sb.AppendLine(
+                $"<meta name=\"{HttpUtility.HtmlEncode(name)}\" content=\"{HttpUtility.HtmlEncode(content)}\">");
         }
     }
 
@@ -73,10 +98,11 @@ public static class HtmlHelperExtensions
     {
         if (!string.IsNullOrWhiteSpace(content))
         {
-            sb.AppendLine($"<meta property=\"{HttpUtility.HtmlEncode(property)}\" content=\"{HttpUtility.HtmlEncode(content)}\">");
+            sb.AppendLine(
+                $"<meta property=\"{HttpUtility.HtmlEncode(property)}\" content=\"{HttpUtility.HtmlEncode(content)}\">");
         }
     }
-    
+
     private static void AddLinkTag(StringBuilder sb, string rel, string? href)
     {
         if (!string.IsNullOrWhiteSpace(href))
@@ -84,5 +110,4 @@ public static class HtmlHelperExtensions
             sb.AppendLine($"<link rel=\"{HttpUtility.HtmlEncode(rel)}\" href=\"{HttpUtility.HtmlEncode(href)}\">");
         }
     }
-
 }
