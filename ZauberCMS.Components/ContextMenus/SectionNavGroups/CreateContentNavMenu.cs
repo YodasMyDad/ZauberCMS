b6b0321433698f5954efc99764ea2fcdf28b2116
Lee <lee@aptitude.co.uk>
2025-03-06 13:31:39 +0000

    Section Nav Group Action Menus functionality added

---
+++ ZauberCMS.Components/ContextMenus/SectionNavGroups/CreateContentNavMenu.cs
@@ -0,0 +1,23 @@
+﻿using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using ZauberCMS.Core;
+using ZauberCMS.Core.Sections.Interfaces;
+
+namespace ZauberCMS.Components.ContextMenus.SectionNavGroups;
+
+public class CreateContentNavMenu : ISectionNavGroupAction
+{
+    public string Text => "Create Content";
+    public string Icon => "add";
+    public string IconColor => string.Empty;
+    public string SectionNavGroupAlias => Constants.Sections.SectionNavGroups.ContentNavGroup;
+    
+    public Task ContextMenuAction(MenuItemEventArgs e, NavigationManager navigationManager, ContextMenuService contextMenuService,
+        IModalService modalService)
+    {
+        contextMenuService.Close();
+        navigationManager.NavigateTo(Urls.AdminCreateContent);
+        return Task.CompletedTask;
+    }
+}
--- ZauberCMS.Core/Sections/Interfaces/ISectionNavGroupAction.cs
+++ ZauberCMS.Core/Sections/Interfaces/ISectionNavGroupAction.cs
@@ -12,22 +12,3 @@ public interface ISectionNavGroupAction
     public string SectionNavGroupAlias { get; }
     Task ContextMenuAction(MenuItemEventArgs e, NavigationManager navigationManager, ContextMenuService contextMenuService, IModalService modalService);
 }
-
-/*void ShowContextMenuWithItems(MouseEventArgs args)
-{
-    ContextMenuService.Open(args,
-        new List<ContextMenuItem> {
-            new ContextMenuItem(){ Text = "Context menu item 1", Value = 1, Icon = "home" },
-            new ContextMenuItem(){ Text = "Context menu item 2", Value = 2, Icon = "search", Disabled = true },
-            new ContextMenuItem(){ Text = "Context menu item 3", Value = 3, Icon = "info" },
-        }, OnMenuItemClick);
-}
-
-void OnMenuItemClick(MenuItemEventArgs args)
-{
-    console.Log($"Menu item with Value={args.Value} clicked");
-    if(!args.Value.Equals(3) && !args.Value.Equals(4))
-    {
-        ContextMenuService.Close();
-    }
-}*/
