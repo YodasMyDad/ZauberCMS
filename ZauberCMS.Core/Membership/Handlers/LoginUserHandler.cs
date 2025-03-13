0c68d7bf0a0731deb3a82b0d42363204d0294fbb
Lee Messenger <lee@aptitude.co.uk>
2025-03-13 07:31:54 +0000

    Adding better error handling to login if there is SMTP issue Adding better UX for when the user needs to confirm their account

--- ZauberCMS.Core/Membership/Handlers/LoginUserHandler.cs
+++ ZauberCMS.Core/Membership/Handlers/LoginUserHandler.cs
@@ -22,69 +22,77 @@ namespace ZauberCMS.Core.Membership.Handlers
             var mediatr = scope.ServiceProvider.GetRequiredService<IMediator>();
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
             var signInManager = scope.ServiceProvider.GetRequiredService<SignInManager<User>>();
-            
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, set lockoutOnFailure: true
             var loginResult = new AuthenticationResult();
-            await signInManager.SignOutAsync();
-            var user = await userManager.FindByEmailAsync(request.Email);
-            if (user != null)
+            try
             {
-                var signInResult = await signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, false);
-                loginResult.Success = signInResult.Succeeded;
+                await signInManager.SignOutAsync();
+                var user = await userManager.FindByEmailAsync(request.Email);
+                if (user != null)
+                {
+                    var signInResult = await signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, false);
+                    loginResult.Success = signInResult.Succeeded;
 
-                if (loginResult.Success)
-                {
-                    var userPrincipal = await signInManager.CreateUserPrincipalAsync(user);
-                    if (request.ReturnUrl.IsNullOrWhiteSpace() && userPrincipal.IsInRole(Constants.Roles.AdminRoleName))
+                    if (loginResult.Success)
                     {
-                        request.ReturnUrl = Urls.AdminBaseUrl;
-                    }
-                    loginResult.NavigateToUrl = request.ReturnUrl;
-                }
-                else
-                {
-                    if (signInResult.IsNotAllowed)
-                    {
-                        if (!await userManager.IsEmailConfirmedAsync(user))
+                        var userPrincipal = await signInManager.CreateUserPrincipalAsync(user);
+                        if (request.ReturnUrl.IsNullOrWhiteSpace() && userPrincipal.IsInRole(Constants.Roles.AdminRoleName))
                         {
-                            loginResult.AddMessage("Email isn't confirmed. Check your inbox for a confirmation email", ResultMessageType.Warning);
-
-                            // Resend confirmation email
-                            var sendConfirmationEmailCommand = new SendEmailConfirmationCommand
-                            {
-                                ReturnUrl = request.ReturnUrl,
-                                User = user
-                            };
-
-                            await mediatr.Send(sendConfirmationEmailCommand, cancellationToken);
+                            request.ReturnUrl = Urls.AdminBaseUrl;
                         }
-
-                        //if (!await _userManager.IsPhoneNumberConfirmedAsync(user))
-                        //{
-                        //      // Phone Number isn't confirmed.
-                        //      // loginResult.FailedReason = "Phone Number isn't confirmed";
-                        //}
-                    }
-                    else if (signInResult.IsLockedOut)
-                    {
-                        logger.LogWarning("User {RequestEmail} account is locked out", request.Email);
-                        loginResult.AddMessage("Account is locked out.", ResultMessageType.Error);
-                    }
-                    else if (signInResult.RequiresTwoFactor)
-                    {
-                        // This is currently not supported
-                        loginResult.NavigateToUrl = $"{Urls.Account.LoginWith2Fa}?returnUrl={request.ReturnUrl}&rememberMe={request.RememberMe}";
+                        loginResult.NavigateToUrl = request.ReturnUrl;
                     }
                     else
                     {
-                        loginResult.AddMessage("Password is incorrect", ResultMessageType.Error);
+                        if (signInResult.IsNotAllowed)
+                        {
+                            if (!await userManager.IsEmailConfirmedAsync(user))
+                            {
+                                loginResult.AddMessage("Email isn't confirmed. Check your inbox for a confirmation email", ResultMessageType.Warning);
+
+                                // Resend confirmation email
+                                var sendConfirmationEmailCommand = new SendEmailConfirmationCommand
+                                {
+                                    ReturnUrl = request.ReturnUrl,
+                                    User = user
+                                };
+
+                                await mediatr.Send(sendConfirmationEmailCommand, cancellationToken);
+                            }
+
+                            //if (!await _userManager.IsPhoneNumberConfirmedAsync(user))
+                            //{
+                            //      // Phone Number isn't confirmed.
+                            //      // loginResult.FailedReason = "Phone Number isn't confirmed";
+                            //}
+                        }
+                        else if (signInResult.IsLockedOut)
+                        {
+                            logger.LogWarning("User {RequestEmail} account is locked out", request.Email);
+                            loginResult.AddMessage("Account is locked out.", ResultMessageType.Error);
+                        }
+                        else if (signInResult.RequiresTwoFactor)
+                        {
+                            // This is currently not supported
+                            loginResult.NavigateToUrl = $"{Urls.Account.LoginWith2Fa}?returnUrl={request.ReturnUrl}&rememberMe={request.RememberMe}";
+                        }
+                        else
+                        {
+                            loginResult.AddMessage("Password is incorrect", ResultMessageType.Error);
+                        }
                     }
                 }
+                else
+                {
+                    loginResult.AddMessage("You are do not have an account, please register", ResultMessageType.Error);
+                }
             }
-            else
+            catch (Exception e)
             {
-                loginResult.AddMessage("You are do not have an account, please register", ResultMessageType.Error);
+               loginResult.AddMessage(e.Message, ResultMessageType.Error);
+               loginResult.Success = false;
+               return loginResult;
             }
 
             return loginResult;
