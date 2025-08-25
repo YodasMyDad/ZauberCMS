38a92ad605c402e108073067f704093b8a2fdd78
Lee Messenger <lee@aptitude.co.uk>
2025-08-25 07:55:06 +0100

    Added `RelatedContentId` to `ZauberContent` table and corresponding migration

---
+++ ZauberCMS.Core/Data/Migrations/PostgreSql/20250825061442_RelatedContentId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZauberCMS.Core.Data.Migrations.PostgreSql
+{
+    /// <inheritdoc />
+    public partial class RelatedContentId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "RelatedContentId",
+                table: "ZauberContent",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RelatedContentId",
+                table: "ZauberContent");
+        }
+    }
+}
---
+++ ZauberCMS.Core/Data/Migrations/SqlServer/20250825061412_RelatedContentId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZauberCMS.Core.Data.Migrations.SqlServer
+{
+    /// <inheritdoc />
+    public partial class RelatedContentId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "RelatedContentId",
+                table: "ZauberContent",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RelatedContentId",
+                table: "ZauberContent");
+        }
+    }
+}
