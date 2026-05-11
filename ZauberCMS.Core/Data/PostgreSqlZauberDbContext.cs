3bfb7d193ff26a370c5b1feda1be08ab1a093cf3
Thomas Rydeen Skyldahl <trs@retus.dk>
2026-05-11 11:32:32 +0200

    Move filtered index to each provider

--- ZauberCMS.Core/Data/PostgreSqlZauberDbContext.cs
+++ ZauberCMS.Core/Data/PostgreSqlZauberDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Options;
+using ZauberCMS.Core.Content.Models;
 using ZauberCMS.Core.Settings;
 
 namespace ZauberCMS.Core.Data;
@@ -23,5 +24,18 @@ public class PostgreSqlZauberDbContext(
 #endif
         options
             .ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Ensure only one current published version per content
+        modelBuilder.Entity<ContentVersion>().HasIndex(x => x.ContentId)
+            .HasFilter("\"IsCurrentPublished\" = true")
+            .HasDatabaseName("IX_ContentVersion_UniqueCurrentPublished").IsUnique();
+        modelBuilder.Entity<ContentVersion>().HasIndex(x => x.ContentId)
+            .HasFilter("\"IsLatestDraft\" = true")
+            .HasDatabaseName("IX_ContentVersion_UniqueLatestDraft").IsUnique();
     }
 }
--- ZauberCMS.Core/Data/SqliteZauberDbContext.cs
+++ ZauberCMS.Core/Data/SqliteZauberDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Options;
+using ZauberCMS.Core.Content.Models;
 using ZauberCMS.Core.Settings;
 
 namespace ZauberCMS.Core.Data;
@@ -24,5 +25,18 @@ public class SqliteZauberDbContext(
         options
             .ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
 
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Ensure only one current published version per content
+        modelBuilder.Entity<ContentVersion>().HasIndex(x => x.ContentId)
+            .HasFilter("\"IsCurrentPublished\" = 1")
+            .HasDatabaseName("IX_ContentVersion_UniqueCurrentPublished").IsUnique();
+        modelBuilder.Entity<ContentVersion>().HasIndex(x => x.ContentId)
+            .HasFilter("\"IsLatestDraft\" = 1")
+            .HasDatabaseName("IX_ContentVersion_UniqueLatestDraft").IsUnique();
     }
 }
