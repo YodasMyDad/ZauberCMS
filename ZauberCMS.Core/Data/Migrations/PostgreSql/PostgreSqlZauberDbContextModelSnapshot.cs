95aede84a23648a92b438118cf47a662abfb85d7
Thomas Rydeen Skyldahl <trs@retus.dk>
2026-06-17 12:41:54 +0200

    regenerated initial migration kept old timestamp

--- ZauberCMS.Core/Data/Migrations/PostgreSql/PostgreSqlZauberDbContextModelSnapshot.cs
+++ ZauberCMS.Core/Data/Migrations/PostgreSql/PostgreSqlZauberDbContextModelSnapshot.cs
@@ -363,7 +363,7 @@ namespace ZauberCMS.Core.Data.Migrations.PostgreSql
                     b.HasIndex("ContentId")
                         .IsUnique()
                         .HasDatabaseName("IX_ContentVersion_UniqueLatestDraft")
-                        .HasFilter("[IsLatestDraft] = 1");
+                        .HasFilter("\"IsLatestDraft\" = true");
 
                     b.HasIndex("CreatedById");
 
